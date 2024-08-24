c85b0819ae8685756f903284b78c2893af1bdcad
navilll <navilfaisal00@gmail.com>
2024-08-24 21:47:56 +0530

    form connected to mongo

--- src/lib/database/db_connect.js
+++ src/lib/database/db_connect.js
@@ -1,9 +1,1 @@
-const username = process.env.myusername;
-const password = process.env.mypassword;
-
-if(!username || !password)
-{
-    throw new Error("Mongo DB Creadential Error");
-}
-
-export const contactString = `mongodb+srv://${username}:${password}@cluster0.1ogeu.mongodb.net/contacts?retryWrites=true&w=majority&appName=Cluster0`
+export const contactString = `mongodb+srv://navilfaisal:navilfaisal18@cluster0.1ogeu.mongodb.net/contacts?retryWrites=true&w=majority&appName=Cluster0`
